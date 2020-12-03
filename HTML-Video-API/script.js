7d4262264099968ae38d9d42d19398d8b7ae3146
David <david.ajayi.anu@gmail.com>
2020-12-03 15:18:44 +0100

    done some stuff

---
+++ HTML-Video-API/script.js
@@ -0,0 +1,44 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stopbtn = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
+
+
+function toggleVideoStatus(){
+    if(video.paused){
+        video.play()
+    }else{
+        video.pause()
+    }
+}
+
+function updatePlayIcon(){
+    if(video.paused){
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    }else{
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+function updateProgress(){
+    return true
+}
+
+function setVideoProgress(){
+    return true
+}
+
+function stopVideo(){
+    video.currentTime = 0
+    video.pause()
+}
+
+video.addEventListener('click', toggleVideoStatus)
+video.addEventListener('pause', updatePlayIcon)
+video.addEventListener('play', updatePlayIcon)
+video.addEventListener('timeupdate', updateProgress)
+
+play.addEventListener('click', toggleVideoStatus)
+stopbtn.addEventListener('click', stopVideo)
+progress.addEventListener('change', setVideoProgress)
